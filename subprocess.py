8dc635a7874dee6ce689e895c28f38acd0a301fc
Daniel Adeboye <adeboyed93@gmail.com>
2024-04-08 13:45:30 +0100

    demo

--- subprocess.py
+++ subprocess.py
@@ -10,8 +10,6 @@ from django.shortcuts import redirect, render
 from django.views.decorators.csrf import csrf_exempt
 from django.template import Template
 
-Test manaa
-
 subprocess.call("grep -R {} .".format(sys.argv[1]), shell=True, cwd="/home/user")
 
 def bad3(request):
