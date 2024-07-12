c4efcd756fb7cc6cd0ebf522aa8b08f2bc2a891d
null (for now) <48127194+kaf-lamed-beyt@users.noreply.github.com>
2024-07-12 19:16:36 +0100

    trigger

--- quality.py
+++ quality.py
@@ -16,3 +16,22 @@ return `
         </body>
       </html>
     `;
+
+return `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src vscode-resource: https:; style-src vscode-resource: https: 'unsafe-inline'; script-src 'nonce-${nonce}' 'unsafe-eval';">
+          <script nonce="${nonce}">
+            console.log("Webview loaded");
+            const vscode = acquireVsCodeApi();
+            vscode.postMessage({ type: 'webviewReady' });
+          </script>
+        </head>
+        <body>
+          <div id="root"></div>
+          <script nonce="${nonce}" src="${scriptSrc}"></script>
+        </body>
+      </html>
+    `;
