e0a62bebfe85cd07d22109490a62fe9cdee4ab0e
Daniel Adeboye <adeboyed93@gmail.com>
2024-10-07 15:48:26 +0100

    Update example.tf

--- example.tf
+++ example.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "foo-bucket" {
   region        = var.region
   bucket        = local.bucket_name
-  force_destroy = false
+  force_destroy = true
 
   tags = {
     Name = "foo-${data.aws_caller_identity.current.account_id}"
