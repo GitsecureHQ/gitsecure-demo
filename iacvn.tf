2e730fc4adaf3639e1f67dbb71fdda81e22f4d0a
Daniel Adeboye <adeboyed93@gmail.com>
2024-10-19 20:59:38 +0100

    Update iacvn.tf

--- iacvn.tf
+++ iacvn.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "foo-bucket" {
   region        = var.region
   bucket        = local.bucket_name
-  force_destroy = true
+  force_destroy = false
 
   tags = {
     Name = "foo-${data.aws_caller_identity.current.account_id}"
